6fea586afd595c367e7a59738432def156664c69
igoreta <ivoronline@gmail.com>
2021-04-13 11:39:26 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_dto_json_object_constructor/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_dto_json_object_constructor/controllers/MyController.java
@@ -2,7 +2,6 @@ package com.ivoronline.springboot_dto_json_object_constructor.controllers;
 
 import com.ivoronline.springboot_dto_json_object_constructor.DTO.PersonDTO;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
