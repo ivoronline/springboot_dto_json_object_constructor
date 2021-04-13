50f07c85a76f0cac8fc48a4e23786821c78a1868
igoreta <ivoronline@gmail.com>
2021-04-13 11:39:13 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_dto_json_object_constructor/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_dto_json_object_constructor/controllers/MyController.java
@@ -1,15 +1,14 @@
 package com.ivoronline.springboot_dto_json_object_constructor.controllers;
 
 import com.ivoronline.springboot_dto_json_object_constructor.DTO.PersonDTO;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MyController {
 
-  @ResponseBody
   @RequestMapping("/AddPerson")
   public String addPerson(@RequestBody PersonDTO personDTO) {
     return personDTO.name + " is " + personDTO.age + " years old";
